581ac71be485035eab19faae6774bdd7deb85d8f
Mujib Ramadhan <53048031+mujibra@users.noreply.github.com>
2026-02-03 15:47:17 +0700

    Optimize package imports for frontend

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  compress: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
+  images: {
+    formats: ["image/avif", "image/webp"],
+  },
+  experimental: {
+    optimizePackageImports: [
+      "@fortawesome/react-fontawesome",
+      "@fortawesome/free-solid-svg-icons",
+      "framer-motion",
+    ],
+  },
   async rewrites() {
     return [
       {
