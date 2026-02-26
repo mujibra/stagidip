2b0b7bf60cd42560bd9e5d935b2e9db6979e8298
Mujib Ramadhan <53048031+mujibra@users.noreply.github.com>
2026-02-26 08:31:46 +0700

    Return canonical replace result from dashboard query updater

---
+++ components/dashboard/useDashboardQueryParams.ts
@@ -0,0 +1,18 @@
+"use client";
+
+import { useCallback, useMemo } from "react";
+
+import { replaceCanonicalHrefIfChanged, type RouterReplaceLike, type SearchParamsLike, searchParamsKey } from "@/components/dashboard/queryParams";
+
+export default function useDashboardQueryParams(pathname: string, searchParams: SearchParamsLike, router: RouterReplaceLike) {
+    const paramsKey = useMemo(() => searchParamsKey(searchParams), [searchParams]);
+
+    return useCallback((mutate: (params: URLSearchParams) => void) => {
+        const currentParams = new URLSearchParams(paramsKey);
+        const nextParams = new URLSearchParams(paramsKey);
+
+        mutate(nextParams);
+
+        return replaceCanonicalHrefIfChanged(pathname, currentParams, nextParams, router);
+    }, [pathname, router, paramsKey]);
+}
